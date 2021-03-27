c2727e395f8365a178b322a0111456519da97412
Melissa Hull <melissahull87@gmail.com>
2021-03-27 18:45:57 -0500

    fghjk

--- models/albummodel.js
+++ models/albummodel.js
@@ -4,7 +4,7 @@ const {
 require('dotenv').config();
 
 const db_url = process.env.DATABASE_URL;
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0" // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0" // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs
 
 const pool = new Pool({
   connectionString: db_url
