07207065a0fb48364fbb5059b3c3c209b734680e
Melissa Hull <melissahull87@gmail.com>
2021-04-02 22:34:01 -0500

    lskdjf

--- models/savemodel.js
+++ models/savemodel.js
@@ -4,7 +4,13 @@ require('dotenv').config();
 const db_url = process.env.DATABASE_URL;
 //process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0" // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs
 
-const pool = new Pool({connectionString: db_url});
+const pool = new Pool({
+  connectionString: db_url,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 
 function savenewphoto(title, hdurl, date, explanation, copyright, callback) {
   console.log("Saving: " + title);
